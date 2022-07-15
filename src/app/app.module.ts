03fb6f55ce3d3d378c6ed479948aecc42ba5a2ff
Florencia <florenciassoledadsosa@gmail.com>
2022-07-15 16:30:39 -0300

    delete list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './component/list/list.component';
 import { WelcomeComponent } from './component/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
     WelcomeComponent
   ],
   imports: [
